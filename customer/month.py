6ecba83a51ed42bddc4b1106d169f12fe22ef943
Farhan Ali Raza <farhanalirazaazeemi@gmail.com>
2021-10-31 20:12:00 +0500

    first commit

---
+++ customer/month.py
@@ -0,0 +1,17 @@
+
+class Mon():
+    def __init__(self, start):
+        self.month = start
+        self.months = ['zero','January','February','March','April','May','June','July','August','September','October','November','December']
+
+    def get_next_month(self):
+        if self.month == 12:
+            self.month = 1
+            return self.month
+        self.month = self.month + 1
+        return self.month
+
+# m = Mon(10)
+# print(m.get_next_month())
+# print(m.get_next_month())
+# print(m.get_next_month())
